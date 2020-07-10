27ea2765c054c7bb399c5649ed821256db732af7
duhao009 <18380581265@163.com>
2020-07-10 22:17:58 +0800

    基础项目

---
+++ src/main/java/com/project/project002/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.project.project002.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by miyakohiroshi on 2020/7/10.
+ */
+@RequestMapping("test")
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test(String string){
+        return  "====" + string;
+    }
+}
