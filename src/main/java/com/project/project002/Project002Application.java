b5a27feaa4e7f43163ebf4c37a280c4e33eb9c03
all_hao_123 <18380581265@163.com>
2020-08-07 16:55:08 +0800

    配置数据库，添加日志输出到控制台

--- src/main/java/com/project/project002/Project002Application.java
+++ src/main/java/com/project/project002/Project002Application.java
@@ -1,9 +1,13 @@
 package com.project.project002;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@MapperScan("com.project.project002.dao")
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Project002Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/project002/controller/TestController.java
+++ src/main/java/com/project/project002/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.project.project002.controller;
 
+import com.project.project002.entity.Type;
+import com.project.project002.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by miyakohiroshi on 2020/7/10.
@@ -11,8 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private TypeService typeService;
+
     @GetMapping("test")
     public String test(String string){
         return  "master123====" + string;
+    }
+
+    @GetMapping("testMysql")
+    public List<Type> testMysql(){
+        return  typeService.getTypeByCode("signUpTimes");
     }
 }
